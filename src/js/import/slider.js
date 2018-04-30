226b2bb6af278910653e027dbac56d20eb84b4e7
kabyr <eugenepro@icloud.com>
2018-04-30 08:32:53 +0300

    upload

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -6,5 +6,30 @@ let introSLider = new Swiper('.intro-slider', {
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
+    clickable: true
+  },
+});
+
+
+let aboutSLider = new Swiper('.about-slider', {
+  speed: 400,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+});
+
+let hitsSLider = new Swiper('.hits-slider', {
+  speed: 400,
+  slidesPerView: 4,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
